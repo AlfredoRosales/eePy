28cb60eff889f858f862dba59c074a51eaa14392
diego.rojas <diego.uni@outlook.com>
2020-09-28 12:55:19 -0500

    segunda base PK in Database

--- models.py
+++ models.py
@@ -1,29 +1,27 @@
 import sqlite3
-miConexion=sqlite3.connect('PrimeraBase') # Se crea la base de datos
-miCursor=miConexion.cursor()        # Se crea el puntero
+miConexion=sqlite3.connect('SegundaBase')
+miCursor=miConexion.cursor()
 
+""" miCursor.execute('''
+    CREATE TABLE PRODUCTOS(
+        CODIGO_ARTICULO VARCHAR(4) PRIMARY KEY,  #Llave Primaria
+        NOMBRE_ARTICULO VARCHAR(50),
+        PRECIO INTEGER,
+        SECCION VARCHAR(20)
+    )  
+''') """
 
-## CREATE
-#miCursor.execute("CREATE TABLE PRODUCTOS(NOMBRE_ARTICULO VARCHAR(50),PRECIO INTEGER, SECCION VARCHAR(20))") Se crea la tabla
-#miCursor.execute("INSERT INTO PRODUCTOS VALUES('BALÓN',15,'DEPORTES')") # Insertando registro
-
-""" variosProductos=[                   # Estos registros están en una TUPLA
-    ('Camiseta',10,'Deportes'),
-    ('Jarrón',10,'Cerámica'),
-    ('Camión',10,'Juguetería')
+""" productos=[
+    ('AR01','Pelota',20,'Juguetería'),
+    ('AR02','Pantalón',15,'Confección'),
+    ('AR03','Destornillador',25,'Ferretería'),
+    ('AR04','Jarrón',45,'Cerámica')
 ]
 
-miCursor.executemany("INSERT INTO PRODUCTOS VALUES (?,?,?)",variosProductos)  # Se inserta lote de registros """
+miCursor.executemany("INSERT INTO PRODUCTOS VALUES (?,?,?,?)", productos) """
 
-## READ
-miCursor.execute("SELECT * FROM PRODUCTOS")  # Consulta de lectura
-
-variosArticulos=miCursor.fetchall()     # Importa todos los registros de la BB.DD en una tupla (variosArticulos)
-#print(variosArticulos)                 # Imprime todos los registros (método 1) (todo el contenido de la Tupla)
-
-for producto in variosArticulos:        # Imprime todos los registros (método 2) (en vertical por el print, sin corchete)
-    print(producto)
+miCursor.execute("INSERT INTO PRODUCTOS VALUES ('AR05','Tren',15,'Juguetería')")
 
 
-miConexion.commit() # Se confirma la inserción del nuevo registro
+miConexion.commit()
 miConexion.close()
